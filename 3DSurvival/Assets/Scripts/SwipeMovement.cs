228e021a23ed9bd5fe7f1bba937bdb876bf97cf3
Nguyen Anh Hao <contactnguyenanhhao@gmail.com>
2025-08-04 21:28:17 +0700

    swipe movement

---
+++ 3DSurvival/Assets/Scripts/SwipeMovement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class SwipeMovement : MonoBehaviour
+{
+    Vector3 startPos, endPos, direction;
+
+    public float swipeRange = 1f;
+
+    Rigidbody rb;
+    Animator anim;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
+        rb.useGravity = false;
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            startPos = Input.GetTouch(0).position;
+        }
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            rb.useGravity = true;
+            anim.SetTrigger("swimming");
+            endPos = Input.GetTouch(0).position;
+            direction = endPos - startPos;
+            rb.AddForce(direction * swipeRange);
+        }
+
+        //smooth rotate to face swipe direction
+        if (direction != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 5f);
+        }
+    }
+}
